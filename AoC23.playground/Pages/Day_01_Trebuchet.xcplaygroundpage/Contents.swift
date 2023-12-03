08ef123d9cc57918ee374b94d6cb3f88aca6882b
Nikolay Dimitrov <tehnsd@gmail.com>
2023-12-03 02:28:03 +0200

    Day 1: Part Two

--- AoC23.playground/Pages/Day_01_Trebuchet.xcplaygroundpage/Contents.swift
+++ AoC23.playground/Pages/Day_01_Trebuchet.xcplaygroundpage/Contents.swift
@@ -4,23 +4,40 @@ guard let fileURL = Bundle.main.url(forResource: "CalibrationDocument", withExte
 let fileContents = try String(contentsOf: fileURL, encoding: String.Encoding.utf8)
 
 let calibrationValues = fileContents.split(separator: "\n")
-print(calibrationValues)
+//print(calibrationValues)
 
-let digits = /[1-9]+/
+let digits = /([1-9]|one|two|three|four|five|six|seven|eight|nine)/
+
+func replace(letterDigit: Substring) -> Substring {
+    letterDigit.replacing("one", with: "1")
+        .replacing("two", with: "2")
+        .replacing("three", with: "3")
+        .replacing("four", with: "4")
+        .replacing("five", with: "5")
+        .replacing("six", with: "6")
+        .replacing("seven", with: "7")
+        .replacing("eight", with: "8")
+        .replacing("nine", with: "9")
+}
 
 let extractedValues = calibrationValues.map { value in
+    print("value: \(value)")
     let digitRange = value.ranges(of: digits)
     guard let firstRange = digitRange.first,
           let lastRange = digitRange.last
     else { fatalError("No range!") }
 
-    let firstValue = value[firstRange].prefix(1)
-    let lastValue = value[lastRange].suffix(1)
+    print("first: \(value[firstRange])")
+    let firstValue = replace(letterDigit: value[firstRange])
+
+    print("last: \(value[lastRange])")
+    let lastValue = replace(letterDigit: value[lastRange])
 
     guard let intValue = Int(firstValue + lastValue) else { fatalError("No Int!") }
-    print(intValue)
+    print("int: \(intValue)")
+    print("---")
     return intValue
 }
 
 let sum = extractedValues.reduce(0, +)
-print(sum)
+print("sum: \(sum)")
